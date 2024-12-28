6e1ab0f4f5ece831346451774769c96fc57c9079
coderMayank69 <mayankbca96325@gmail.com>
2024-12-28 16:36:15 +0530

    Add files via upload

---
+++ header.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include "mayank.h"
+int main(){
+    mayank(6,7);
+    return 0;
+}
---
+++ mayank.h
@@ -0,0 +1,9 @@
+#ifndef mayank_h // if not defined
+#define mayank_h // define it
+#define age 18
+void mayank(int a, int b){
+    printf("Hello, I am Mayank\nA passionate programmer\nMy age is %d\n", age);
+    printf("The sum of %d and %d is %d\n", a, b, a+b);
+}; // function prototype
+
+#endif // end if
